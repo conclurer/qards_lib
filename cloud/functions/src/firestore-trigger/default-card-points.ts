317ff1b5a17c992f27ec55c41e113ddb12a4ea6b
Aaron Czichon <aaron@inoverse.de>
2018-05-26 02:39:21 +0200

    Removed unused imports

--- cloud/functions/src/firestore-trigger/default-card-points.ts
+++ cloud/functions/src/firestore-trigger/default-card-points.ts
@@ -1,15 +1,12 @@
 import * as functions from 'firebase-functions';
 import { DocumentSnapshot } from '@google-cloud/firestore';
 import { EventContext } from 'firebase-functions';
-import * as firebaseAdmin from 'firebase-admin';
 
 export function defaultCardPoints() {
     return functions
         .firestore
         .document('cards/{cardId}')
         .onCreate((snapshot: DocumentSnapshot, context: EventContext) => {
-            const card = snapshot.data();
-
             return snapshot.ref.set({
                 score: 10
             }, { merge: true });
