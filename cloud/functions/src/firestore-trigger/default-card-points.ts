8064fa6e7d6d4a5dda3f56c5a4377114b65f4b7b
Aaron Czichon <aaron@inoverse.de>
2018-05-26 02:26:16 +0200

    Added cloud function for default card value

---
+++ cloud/functions/src/firestore-trigger/default-card-points.ts
@@ -0,0 +1,18 @@
+import * as functions from 'firebase-functions';
+import { DocumentSnapshot } from '@google-cloud/firestore';
+import { EventContext } from 'firebase-functions';
+import { Card } from '../interfaces/card.interface';
+import * as firebaseAdmin from 'firebase-admin';
+
+export const defaultCardPoints = () => {
+    return functions
+        .firestore
+        .document('cards/{cardId}')
+        .onCreate((snapshot: DocumentSnapshot, context: EventContext) => {
+            const card = snapshot.data();
+
+            return snapshot.ref.set({
+                score: 10
+            }, { merge: true });
+        });
+} 
---
+++ cloud/functions/src/index.ts
@@ -0,0 +1,5 @@
+import * as functions from 'firebase-functions';
+import { defaultCardPoints } from './firestore-trigger/default-card-points';
+
+// Function registration
+exports.defaultCardPoints = defaultCardPoints;
